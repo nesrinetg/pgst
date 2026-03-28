10d54e76819ca28acc8a153b1a54346683590c0c
Anestassia <oustaniassia204@gmail.com>
2026-03-28 06:26:14 -0700

    message

--- routes/web.php
+++ routes/web.php
@@ -4,66 +4,46 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-
-
-
-Route::get('/login', function () {
-    return view('authentification/login.php');
-});
-*/
+// Page d'accueil
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
 
-Route::get('/', function () {
-    return view('dashbord');
-})->name('dashbord');
-
-Route::get('/sous-traitants', function () {
-    return view('sous_traitants');
-})->name('sous-traitants.index');
-
-Route::get('/zones', function () {
-    return view('zones');
-})->name('zones.index');
-
-Route::get('/tickets', function () {
-    return view('tickets');
-})->name('tickets.index');
-
-Route::get('/kpi-rapports', function () {
-    return view('kpi_rapports');
-})->name('kpi-rapports.index');
-
-Route::get('/parametres', function () {
-    return view('parametres');
-})->name('parametres.index');
-
-Route::get('/gestion_utilisateurs', function () {
-    return view('gestion_utilisateurs');
-})->name('gestion_utilisateurs.index');
-
-
-
+// Login
 Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 
+// Logout
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+// Dashboard (protégé par auth)
 Route::get('/dashboard', function () {
     return view('dashbord');
 })->middleware('auth');
 
+
+// Autres pages protégées
+Route::get('/sous-traitants', function () {
+    return view('sous_traitants');
+})->middleware('auth')->name('sous-traitants.index');
+
+Route::get('/zones', function () {
+    return view('zones');
+})->middleware('auth')->name('zones.index');
+
+Route::get('/tickets', function () {
+    return view('tickets');
+})->middleware('auth')->name('tickets.index');
+
+Route::get('/kpi-rapports', function () {
+    return view('kpi_rapports');
+})->middleware('auth')->name('kpi-rapports.index');
+
+Route::get('/parametres', function () {
+    return view('parametres');
+})->middleware('auth')->name('parametres.index');
+
+Route::get('/gestion_utilisateurs', function () {
+    return view('gestion_utilisateurs');
+})->middleware('auth')->name('gestion_utilisateurs.index');
